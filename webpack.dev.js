c52acb345b32e5e9503c8d846657ef2a44a0dc12
liyong6 <liyong6@staff.sina.com.cn>
2018-11-19 19:13:07 +0800

    init

--- webpack.dev.js
+++ webpack.dev.js
@@ -40,6 +40,10 @@ module.exports = {
 			{
                 test: /\.css$/,
                 use: ['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(gif|jpg|png|woff|svg|eot|ttf)\??.*$/,
+                use: 'url-loader'
             }
 		]
 	},
